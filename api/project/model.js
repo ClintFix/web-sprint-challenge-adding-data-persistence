ef05c6772dbfb1046f3f59c006dcf0f776d02103
ClintFix <clint.fix@gmail.com>
2021-04-16 10:10:10 -0600

    endpoint/model for get all projects working

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,18 @@
-// build your `Project` model here
+
+const db = require('../../data/dbConfig');
+
+async function getAll() {
+    const projects = await db("projects as p");
+    const projectsWithBoolean = projects.map(project => {
+        if (project.project_completed === 0 || !project.project_completed) {
+            return {...project, "project_completed": false};
+        } else {
+            return {...project, "project_completed": true};
+        }
+    });
+    return projectsWithBoolean; 
+}
+
+module.exports = {
+    getAll,
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,15 @@
-// build your `/api/projects` router here
+
+const express = require('express');
+const Projects = require("./model");
+
+const router = express.Router();
+
+router.get("/", (_,res, next) => {
+    Projects.getAll()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => next(err))
+}); 
+
+module.exports = router;
