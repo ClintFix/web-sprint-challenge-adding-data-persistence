44d6f995f31f822f6ab0580e3186993299000a32
ClintFix <clint.fix@gmail.com>
2021-04-16 09:01:40 -0600

    tasks table knex code done

--- data/migrations/20210416144919_first-migration.js
+++ data/migrations/20210416144919_first-migration.js
@@ -5,17 +5,32 @@ exports.up = function(knex) {
             tbl.increments("project_id");
             tbl.string("project_name", 128).notNullable();
             tbl.string("project_description");
-            tbl.boolean("project_completed").notNullable().defaultTo(0);
+            tbl.boolean("project_completed").defaultTo(0);
         })
-        .createTable()
-        .createTable()
+        .createTable("resources", tbl => {
+            tbl.increments("resource_id");
+            tbl.string("resource_name", 80).notNullable().unique();
+            tbl.string("resource_description");
+        })
+        .createTable("tasks", tbl => {
+            tbl.increments("task_id");
+            tbl.string("task_description").notNullable();
+            tbl.string("task_notes");
+            tbl.boolean("task_completed").defaultTo(0);
+            tbl.integer("project_id")
+                .unsigned()
+                .notNullable()
+                .references("project_id")
+                .inTable("projects")
+                .onDelete("RESTRICT")
+        })
         .createTable()
 };
 
 exports.down = function(knex) {
     return knex.schema
         .dropTableIfExists()
-        .dropTableIfExists()
-        .dropTableIfExists()
+        .dropTableIfExists("tasks")
+        .dropTableIfExists("resources")
         .dropTableIfExists("projects")
 };
